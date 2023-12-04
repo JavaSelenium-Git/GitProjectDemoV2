932cd1e8a614b9e3af855198c269cd909ecdd8b6
Codewithmanasa <Codewithmanasa@gmail.com>
2023-12-04 16:07:07 +0530

    Commit from Repo B

--- src/com/samplecode/Test.java
+++ src/com/samplecode/Test.java
@@ -3,7 +3,16 @@ package com.samplecode;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.print("Class to test for code review and Merge");
+<<<<<<< HEAD
+	System.out.print("Class to test for code review and Merge");
+=======
+                int a = new Scanner(System.in).nextInt();
+		System.out.print(a);
+		System.out.print("Class to test and Merge");
+		
+		
+		
+>>>>>>> 14b07f4 (Commit from Repo B)
 	}
 
 }
